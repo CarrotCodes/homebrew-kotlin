216d03d4c0be553ec9a1855b56670242631eb590
Sky Welch <git@bunnies.io>
2016-12-01 16:39:03 +0000

    Add Kotlin 1.1-M03 formula

---
+++ Formula/kotlin11M03.rb
@@ -0,0 +1,27 @@
+class Kotlin11M03 < Formula
+  desc "Statically typed programming language for the JVM"
+  homepage "https://kotlinlang.org/"
+  url "https://github.com/JetBrains/kotlin/releases/download/v1.1-M03/kotlin-compiler-1.1-M03.zip"
+  sha256 "55f630899df71894d01f1b25a048bbcc592e28e2545f955eb1034be0eb12c1a7"
+
+  bottle :unneeded
+
+  conflicts_with "kotlin", :because => "Differing version of same formula"
+
+  def install
+    libexec.install %w[bin lib]
+    rm Dir["#{libexec}/bin/*.bat"]
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    (testpath/"test.kt").write <<-EOS.undent
+      fun main(args: Array<String>) {
+        println("Hello World!")
+      }
+    EOS
+    system "#{bin}/kotlinc", "test.kt", "-include-runtime", "-d", "test.jar"
+    system "#{bin}/kotlinc-js", "test.kt", "-output", "test.js"
+    system "#{bin}/kotlinc-jvm", "test.kt", "-include-runtime", "-d", "test.jar"
+  end
+end
