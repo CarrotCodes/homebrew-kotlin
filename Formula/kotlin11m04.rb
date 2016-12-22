d268b0822aa372c03328f399600c15becf8da606
Sky Welch <git@bunnies.io>
2016-12-22 19:30:32 +0000

    Add Kotlin 1.1-M04

---
+++ Formula/kotlin11m04.rb
@@ -0,0 +1,27 @@
+class Kotlin11m04 < Formula
+  desc "Statically typed programming language for the JVM"
+  homepage "https://kotlinlang.org/"
+  url "https://github.com/JetBrains/kotlin/releases/download/v1.1-M04/kotlin-compiler-1.1-M04.zip"
+  sha256 "ef5fded3cfca97ddc97e6ebf306fc6be57dc4c447e00f9290e2db05713da0003"
+
+  bottle :unneeded
+
+  conflicts_with "kotlin", :because => "Differing version of same formula"
+
+  def install
+    libexec.install %w[bin lib]
+    rm Dir["#{libexec}/bin/*.bat"]
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    (testpath/"test.kt").write <<-EOS.undent
+      fun main(args: Array<String>) {
+        println("Hello World!")
+      }
+    EOS
+    system "#{bin}/kotlinc", "test.kt", "-include-runtime", "-d", "test.jar"
+    system "#{bin}/kotlinc-js", "test.kt", "-output", "test.js"
+    system "#{bin}/kotlinc-jvm", "test.kt", "-include-runtime", "-d", "test.jar"
+  end
+end
