2b10c66b880304481babbe8577ce706aadd0e33b
Sky Welch <git@bunnies.io>
2017-02-05 20:30:18 +0000

    Add Kotlin 1.1-beta2

---
+++ Formula/kotlin11beta2.rb
@@ -0,0 +1,27 @@
+class Kotlin11beta2 < Formula
+  desc "Statically typed programming language for the JVM"
+  homepage "https://kotlinlang.org/"
+  url "https://github.com/JetBrains/kotlin/releases/download/v1.1-beta2/kotlin-compiler-1.1-beta2.zip"
+  sha256 "dd53c13a9ba6ddb8af90fd56c2948cefe736122eb8dea14c36388af7d2c1c433"
+
+  bottle :unneeded
+
+  conflicts_with "kotlin", :because => "Differing version of same formula"
+
+  def install
+    libexec.install %w[bin lib]
+    rm Dir["#{libexec}/bin/*.bat"]
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    (testpath/"test.kt").write <<-EOS.undent
+      fun main(args: Array<String>) {
+        println("Hello World!")
+      }
+    EOS
+    system "#{bin}/kotlinc", "test.kt", "-include-runtime", "-d", "test.jar"
+    system "#{bin}/kotlinc-js", "test.kt", "-output", "test.js"
+    system "#{bin}/kotlinc-jvm", "test.kt", "-include-runtime", "-d", "test.jar"
+  end
+end
